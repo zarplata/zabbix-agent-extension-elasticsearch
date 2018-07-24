8dbc53b55bf9360799a0c0b9d47a7afb9fc358a9
Andrey Platonov <poluandrey@gmail.com>
2018-07-24 19:13:44 +0700

    modify PKGBUILD

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eou pipefail
+
+package_name="zabbix-agent-extension-elasticsearch"
+
+rm -rf *.tar.xz
+makepkg -Cod; PKGVER=$(cd $(pwd)/src/$package_name/ && make ver) makepkg -esd
+
