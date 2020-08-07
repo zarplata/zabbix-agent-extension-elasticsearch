73b6cd1c061e050b3fd82139f8ea20a8d7b41b31
Dmitriy Efremov <me@arren.ru>
2020-08-07 16:33:17 +0700

    Updated PKGBUILD

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-package_name="zabbix-agent-extension-elasticsearch"
-rm -rf *.tar.xz
-makepkg -Cod; PKGVER=$(cd $(pwd)/src/$package_name/ && make ver) makepkg -esd
-
